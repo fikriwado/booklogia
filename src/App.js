3023b1209f70ae9da21b51a495595af0351bd51b
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-06-17 01:55:57 +0700

    add footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Forum from './pages/Forum';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Forum from "./pages/Forum";
+import "./App.css";
 
 function App() {
-	return (
-		<div className="App">
-            <Router>
-                <Navbar />
-                <Switch>
-					<Route exact path="/" component={Home} />
-					<Route path="/about" component={About} />
-					<Route path="/forum" component={Forum} />
-                </Switch>
-                <Footer />
-            </Router>
-        </div>
-	);
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/forum" component={Forum} />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,47 +1,47 @@
-import React from 'react';
-import logo from '../../logo.svg';
-import { Link } from 'react-router-dom';
-import { Container, Navbar as NavbarBootstrap, Nav } from 'react-bootstrap';
+import React from "react";
+import logo from "../../logo.svg";
+import { Link } from "react-router-dom";
+import { Container, Navbar as NavbarBootstrap, Nav } from "react-bootstrap";
 
 function Navbar() {
-    return (
-        <>
-            <NavbarBootstrap bg="dark" variant="dark" expand="lg">
-                <Container>
-                    <NavbarBootstrap.Brand href="/">
-                        <img
-                            alt=""
-                            src={logo}
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top"
-                        />{' '}
-                        Booklogia
-                    </NavbarBootstrap.Brand>
-                    <NavbarBootstrap.Toggle aria-controls="basic-navbar-nav" />
-                    <NavbarBootstrap.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link>
-                                <Link to="/" className="nav-link">
-                                    Home
-                                </Link>
-                            </Nav.Link>
-                            <Nav.Link>
-                                <Link to="/about" className="nav-link">
-                                    About
-                                </Link>
-                            </Nav.Link>
-                            <Nav.Link>
-                                <Link to="/forum" className="nav-link">
-                                    Forum
-                                </Link>
-                            </Nav.Link>
-                        </Nav>
-                    </NavbarBootstrap.Collapse>
-                </Container>
-            </NavbarBootstrap>
-        </>
-    );
+  return (
+    <>
+      <NavbarBootstrap bg="dark" variant="dark" expand="lg">
+        <Container>
+          <NavbarBootstrap.Brand href="/">
+            <img
+              alt=""
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Booklogia
+          </NavbarBootstrap.Brand>
+          <NavbarBootstrap.Toggle aria-controls="basic-navbar-nav" />
+          <NavbarBootstrap.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link>
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/about" className="nav-link">
+                  About
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/forum" className="nav-link">
+                  Forum
+                </Link>
+              </Nav.Link>
+            </Nav>
+          </NavbarBootstrap.Collapse>
+        </Container>
+      </NavbarBootstrap>
+    </>
+  );
 }
 
 export default Navbar;
