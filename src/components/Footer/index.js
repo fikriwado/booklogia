229912c2c9fc83aba5031002a7986c081ce17f5d
zeldaababil19 <zeldaababil01@gmail.com>
2022-06-18 15:47:13 +0700

    google font

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,51 +1,51 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import logo from "../../assets/Images/BookLogia.png";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import logo from '../../assets/images/BookLogia.png';
 
 function Footer() {
   return (
-    <footer className="bg-dark text-light py-5">
+    <footer className='bg-dark text-light py-5'>
       <Container>
         <Row>
           <Col md={3}>
-            <h5 className="text-uppercase mb-4 font-weight-bold">
+            <h5 className='text-uppercase mb-4 font-weight-bold'>
               Company name
             </h5>
-            <img className="d-block mb-4 mx-auto" src={logo} alt=" " />
+            <img className='d-block mb-4 mx-auto' src={logo} alt=' ' />
           </Col>
 
-          <hr className="w-100 bg-secondary d-md-none" />
+          <hr className='w-100 bg-secondary d-md-none' />
 
-          <Col md={3} className="ps-xl-5">
-            <h5 className="text-uppercase mb-4 font-weight-bold">Popular</h5>
-            <ul className="list-unstyled">
-              <li className="mb-3">
-                <a href="/#" className="text-light text-decoration-none">
+          <Col md={3} className='ps-xl-5'>
+            <h5 className='text-uppercase mb-4 font-weight-bold'>Popular</h5>
+            <ul className='list-unstyled'>
+              <li className='mb-3'>
+                <a href='/#' className='text-light text-decoration-none'>
                   Becoming by Michelle Obama
                 </a>
               </li>
-              <li className="mb-3">
-                <a href="/#" className="text-light text-decoration-none">
+              <li className='mb-3'>
+                <a href='/#' className='text-light text-decoration-none'>
                   Dear Tomorrow
                 </a>
               </li>
-              <li className="mb-3">
-                <a href="/#" className="text-light text-decoration-none">
+              <li className='mb-3'>
+                <a href='/#' className='text-light text-decoration-none'>
                   Merdeka Sejak Hati
                 </a>
               </li>
-              <li className="mb-3">
-                <a href="/#" className="text-light text-decoration-none">
+              <li className='mb-3'>
+                <a href='/#' className='text-light text-decoration-none'>
                   A Wrinkle in Time
                 </a>
               </li>
             </ul>
           </Col>
 
-          <hr className="w-100 bg-secondary d-md-none" />
+          <hr className='w-100 bg-secondary d-md-none' />
 
-          <Col md={3} className="pe-xl-5 ">
-            <h5 className="text-uppercase mb-4 font-weight-bold">About</h5>
+          <Col md={3} className='pe-xl-5 '>
+            <h5 className='text-uppercase mb-4 font-weight-bold'>About</h5>
             <p>
               Buku sering disebut sebagai jendela dunia karena dengan membaca
               buku kita dapat mengetahui apa saja yang ada di dunia ini baik
@@ -53,41 +53,41 @@ function Footer() {
             </p>
           </Col>
 
-          <hr className="w-100 bg-secondary d-md-none" />
+          <hr className='w-100 bg-secondary d-md-none' />
 
-          <Col md={3} className="ps-xl-4">
-            <h5 className="text-uppercase mb-4 font-weight-bold">Contact</h5>
-            <ul className="list-unstyled">
-              <li className="mb-3">
-                <a href="/#" className="text-light text-decoration-none">
-                  <i className="fas fa-home mr-3 text-white"></i>{" "}
+          <Col md={3} className='ps-xl-4'>
+            <h5 className='text-uppercase mb-4 font-weight-bold'>Contact</h5>
+            <ul className='list-unstyled'>
+              <li className='mb-3'>
+                <a href='/#' className='text-light text-decoration-none'>
+                  <i className='fas fa-home mr-3 text-white'></i>{' '}
                   <span>Indonesia</span>
                 </a>
               </li>
-              <li className="mb-3">
-                <a href="/#" className="text-light text-decoration-none">
-                  <i className="fas fa-envelope mr-3 text-white"></i>{" "}
-                  <span className=" m-lg-1"> booklogia@gmail.com </span>
+              <li className='mb-3'>
+                <a href='/#' className='text-light text-decoration-none'>
+                  <i className='fas fa-envelope mr-3 text-white'></i>{' '}
+                  <span className=' m-lg-1'> booklogia@gmail.com </span>
                 </a>
               </li>
-              <li className="mb-3">
-                <a href="/#" className="text-light text-decoration-none">
-                  <i className="fas fa-phone mr-3 text-white"></i>{" "}
-                  <span className=" m-lg-1"> +6282131715623 </span>
+              <li className='mb-3'>
+                <a href='/#' className='text-light text-decoration-none'>
+                  <i className='fas fa-phone mr-3 text-white'></i>{' '}
+                  <span className=' m-lg-1'> +6282131715623 </span>
                 </a>
               </li>
-              <li className="mb-3">
-                <form action="#" class="subscribe">
+              <li className='mb-3'>
+                <form action='#' className='subscribe'>
                   <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Enter your e-mail"
+                    type='text'
+                    className='form-control'
+                    placeholder='Enter your e-mail'
                   />
 
                   <input
-                    type="submit"
-                    class="btn btn-submits text-white"
-                    value="Send"
+                    type='submit'
+                    className='btn btn-submits text-white'
+                    value='Send'
                   />
                 </form>
               </li>
@@ -95,50 +95,50 @@ function Footer() {
           </Col>
         </Row>
 
-        <hr className="my-3 bg-secondary " />
+        <hr className='my-3 bg-secondary ' />
 
-        <Row className="align-items-center">
-          <Col md={6} className="text-center text-md-start">
+        <Row className='align-items-center'>
+          <Col md={6} className='text-center text-md-start'>
             <span>&copy; 2022 Copyright:</span>
             <a
-              className="text-white text-decoration-none"
-              href="https://google.com/"
+              className='text-white text-decoration-none'
+              href='https://google.com/'
             >
               BookLogia
             </a>
           </Col>
 
-          <Col md={6} className="text-center text-md-end">
+          <Col md={6} className='text-center text-md-end'>
             <a
-              className="btn m-1 text-white"
-              role="button"
-              href="https://www.instagram.com/"
+              className='btn m-1 text-white'
+              role='button'
+              href='https://www.instagram.com/'
             >
-              <i className="fab fa-instagram  icon-media-footer"></i>
+              <i className='fab fa-instagram  icon-media-footer'></i>
             </a>
 
             <a
-              className="btn m-1 text-white"
-              role="button"
-              href="https://www.facebook.com/"
+              className='btn m-1 text-white'
+              role='button'
+              href='https://www.facebook.com/'
             >
-              <i className="fab fa-facebook-f icon-media-footer"></i>
+              <i className='fab fa-facebook-f icon-media-footer'></i>
             </a>
 
             <a
-              className="btn m-1 text-white"
-              role="button"
-              href="https://twitter.com/"
+              className='btn m-1 text-white'
+              role='button'
+              href='https://twitter.com/'
             >
-              <i className="fab fa-twitter icon-media-footer"></i>
+              <i className='fab fa-twitter icon-media-footer'></i>
             </a>
 
             <a
-              className="btn m-1 text-white"
-              role="button"
-              href="https://google.com/"
+              className='btn m-1 text-white'
+              role='button'
+              href='https://google.com/'
             >
-              <i className="fab fa-google icon-media-footer"></i>
+              <i className='fab fa-google icon-media-footer'></i>
             </a>
           </Col>
         </Row>
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,75 @@
-import { Button, Col, Container } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import './App.css';
 
 function Login() {
   return (
     <Container className='py-3'>
-      {' '}
-      <Button as={Col} variant='primary'>
+      <Row>
+        <Col md={6} className='mx-auto'>
+          <h1 style={{ fontFamily: "'Montserrat', sans-serif" }}>Login</h1>
+          <form>
+            <div className='form-group'>
+              <label htmlFor='exampleInputEmail1'>Email address</label>
+              <input
+                type='email'
+                className='form-control'
+                id='exampleInputEmail1'
+                aria-describedby='emailHelp'
+                placeholder='Enter email'
+              />
+              <small id='emailHelp' className='form-text text-muted'>
+                We'll never share your email with anyone else.
+              </small>
+            </div>
+            <div className='form-group'>
+              <label htmlFor='exampleInputPassword1'>Password</label>
+              <input
+                type='password'
+                className='form-control'
+                id='exampleInputPassword1'
+                placeholder='Password'
+              />
+            </div>
+            <Button type='submit' className='btn btn-primary'>
+              Submit
+            </Button>
+          </form>
+        </Col>
+        <Col md={6} className='mx-auto'>
+          <h1>Login</h1>
+          <form>
+            <div className='form-group'>
+              <label htmlFor='exampleInputEmail1'>Email address</label>
+              <input
+                type='email'
+                className='form-control'
+                id='exampleInputEmail1'
+                aria-describedby='emailHelp'
+                placeholder='Enter email'
+              />
+              <small id='emailHelp' className='form-text text-muted'>
+                We'll never share your email with anyone else.
+              </small>
+            </div>
+            <div className='form-group'>
+              <label htmlFor='exampleInputPassword1'>Password</label>
+              <input
+                type='password'
+                className='form-control'
+                id='exampleInputPassword1'
+                placeholder='Password'
+              />
+            </div>
+            <Button type='submit' className='btn btn-primary'>
+              Submit
+            </Button>
+          </form>
+        </Col>
+      </Row>{' '}
+      {/* <Button className='btn-login' as={Col} variant='primary'>
         Button #1
       </Button>
-      <h1>Ini adalah Login!</h1> <button>ini button</button>
+      <h1>Ini adalah Login!</h1> <button>ini button</button> */}
     </Container>
   );
 }
