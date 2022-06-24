1bc549715d1d6aecf2dcd04f14b51bd4a4b5dea6
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-06-25 04:05:44 +0700

    about

---
+++ src/pages/About/Detail/Artikel.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+function Artikel() {
+  return (
+    <div>
+      <section className="padding-y-xxl position-relative z-index-1 pt-3 pb-5">
+        <div className="container max-width-adaptive-sm position-relative z-index-2">
+          <div className="text-component margin-bottom-sm pt-5">
+            <h1>Solusi bertahan di industry 4.0 </h1>
+            <p>Booklogia / 10 Sep 2021</p>
+          </div>
+        </div>
+
+        <div className="container pt-3">
+          <img
+            src="https://cdn0-production-images-kly.akamaized.net/oqVK1UmRUMoQzNjpGNo2ATFQXzE=/0x72:999x635/640x360/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3613377/original/028028000_1635234422-shutterstock_1702787302.jpg"
+            alt=""
+            className="image-banner "
+          />
+        </div>
+
+        <div className="container pt-4">
+          <p>Hallo guys.</p>
+          <p className="text-break">
+            Seringkali infrastruktur yang luas yang mendasari peradaban industri
+            mengalami transformasi yang dramatis. Tetapi perubahan seperti itu
+            tampaknya terjadi sekarang. Dalam gelombang besar perubahan
+            teknologi, sensor menyebar melalui pabrik dan gudang, perangkat
+            lunak memprediksi perlunya perawatan sebelum mesin rusak, jaringan
+            listrik dan dok pemuatan menjadi cerdas, dan komponen yang dirancang
+            khusus sedang diproduksi sesuai permintaan. Para pemimpin revolusi
+            industri berikutnya adalah perusahaan yang membuat kemajuan di
+            bidang-bidang seperti robotika, pembelajaran mesin, fabrikasi
+            digital.
+          </p>
+          <p className="text-break">
+            Karena semua teknologi ini memperkuat dampak orang lain, mereka
+            mengarah ke tingkat kemahiran baru, dan jenis peluang dan tantangan
+            baru untuk bisnis dan untuk masyarakat luas. Salah satu indikator
+            kunci adalah bahwa batas-batas konvensional di antara
+            industri-industri sedang mengikis. Semakin sulit untuk membedakan
+            antara, katakanlah, perusahaan telekomunikasi dan produsen hiburan,
+            atau antara bank ritel dan toko ritel. Hubungan di antara pemasok,
+            produsen, dan konsumen juga kabur, lebih cepat daripada banyak
+            pengambil keputusan bisnis yang dipersiapkan.
+          </p>
+
+          <p className="text-break">
+            Widi juga menjelaskan bahwa penerapan teknologi informasi secara
+            digital pada perusahaan bukan hanya sekadar keinginan yang kuat ,
+            namun juga memerlukan blueprint, dokumentasi yang jelas tentang visi
+            dan misi strategis perusahaan serta pendekatan secara bertahap.
+          </p>
+          <p className="text-break">
+            "Selain memperkuat fondasi transformasi digital perusahaan, tidak
+            hanya dengan mendapatkan wawasan tentang teknologi digital terbaru,
+            tetapi juga menemukan cara bagaimana tetap relevan dan memimpin
+            persaingan di era revolusi industri 4.0 ini," ujar Widi menandaskan.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Artikel;
